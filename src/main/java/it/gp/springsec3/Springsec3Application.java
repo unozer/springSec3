a1d21cd9129d726ad33f67ef5dd24d2f8f3d46c1
unozer <40232537+unozer@users.noreply.github.com>
2024-07-29 17:21:02 +0200

    First Commit

---
+++ src/main/java/it/gp/springsec3/Springsec3Application.java
@@ -0,0 +1,13 @@
+package it.gp.springsec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsec3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsec3Application.class, args);
+	}
+
+}
